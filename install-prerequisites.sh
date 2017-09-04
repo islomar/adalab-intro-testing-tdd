20dbebb5575010b1fd8a6a8c007e27aa6507c8c7
Isidro López <islomar@gmail.com>
2017-09-04 16:11:01 +0200

    Add node and npm installation commands for Ubuntu

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 #By default, you don't need to install this
+sudo apt-get update
+sudo apt-get install nodejs
 npm install babel-core babel-loader babel-preset-es2015 webpack webpack-dev-server --save
 npm install mocha chai karma karma-chai karma-mocha karma-webpack karma-phantomjs-launcher --save-dev
