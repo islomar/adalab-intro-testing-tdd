b512ca7f7486c20626703969d101fdcdab673dd2
Isidro López <islomar@gmail.com>
2017-09-04 16:11:24 +0200

    Update install-prerequisites.sh

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -2,6 +2,6 @@
 
 #By default, you don't need to install this
 sudo apt-get update
-sudo apt-get install nodejs
+sudo apt-get install nodejs npm
 npm install babel-core babel-loader babel-preset-es2015 webpack webpack-dev-server --save
 npm install mocha chai karma karma-chai karma-mocha karma-webpack karma-phantomjs-launcher --save-dev
